f9763c61773170151571dfec82e74f788480f635
Katy Coe <djkaty@start.no>
2018-03-09 00:54:47 +0100

    Add project files.

---
+++ Firestone/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Firestone
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
