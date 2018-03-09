4ff5794c50078e93be1306ab939e0718b93d5646
Katy Coe <djkaty@start.no>
2018-03-09 01:19:01 +0100

    Add log4net boilerplate code and dependencies

--- Firestone/Program.cs
+++ Firestone/Program.cs
@@ -2,6 +2,9 @@
 using System.IO;
 using System.Reflection;
 using Microsoft.Extensions.Configuration;
+using log4net;
+
+[assembly: log4net.Config.XmlConfigurator(ConfigFile="log4net.config.xml", Watch=true)]
 
 namespace Firestone
 {
@@ -12,12 +15,21 @@ namespace Firestone
         /// </summary>
         public static IConfiguration Configuration { get; set; }
 
+        // Logger
+        private static readonly ILog Log = LogManager.GetLogger(MethodBase.GetCurrentMethod().DeclaringType);
+
         public static void Main(string[] args) {
+            Log.Info("Server starting up");
+
             var config = new ConfigurationBuilder()
                 .SetBasePath(Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location))
                 .AddJsonFile("firestone-conf.json");
 
             Configuration = config.Build();
+
+            Log.Info("Configuration loaded successfully");
+
+            Log.Info("Server shutting down");
         }
     }
 }
